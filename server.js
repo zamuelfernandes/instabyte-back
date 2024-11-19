2de69db8c69ff545bfe4e0bca0a64d874826d9b1
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-11-19 09:22:37 -0300

    Aprofundando nos conceitos de API

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 import express from "express";
 
+const posts_array = [
+  {
+    id: 0,
+    descricao: "Gato curioso olhando para a câmera",
+    imagem: "https://placekitten.com/400/200"
+  },
+  {
+    id: 1,
+    descricao: "Paisagem com um gato",
+    imagem: "https://placekitten.com/g/200/300"
+  },
+  {
+    id: 2,
+    descricao: "Gato brincando com um novelo de lã",
+    imagem: "https://placekitten.com/200/300"
+  }
+];
+
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("Server listening...");
+  console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Welcome to server!");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts_array);
+});
+
+function buscarPostPorId(id) {
+  return posts_array.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorId(req.params.id);
+  res.status(200).json(posts_array[index]);
 });
